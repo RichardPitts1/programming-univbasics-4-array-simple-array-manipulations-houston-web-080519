af43e243c91a939dc0b529090fdbe019858fbe20
Richard Pitts <richardepitts@gmail.com>
2019-07-22 20:32:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,8 +14,8 @@ def pop_with_args(array, argument = 2)
   array.pop(argument)
 end 
 
-def using_shift(array, string)
-  array.shift(string)
+def using_shift(array)
+  array.shift
 end
 
 def shift_with_args(array, argument = 2)
